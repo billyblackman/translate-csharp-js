3982c2bab957ed013324d7d921f728f0c50f950d
Billy Blackman <blackmanbilly@gmail.com>
2020-05-22 16:17:10 -0500

    Completed place names assignment

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,4 +1,34 @@
-function main() {
+ffunction main() {
+  // Put your code here
+
+  const names =
+    ["Nashville",
+      "Hong Kong",
+      "The back yard",
+      "Earth",
+      "London",
+      "The mall",
+      "Ryman Auditorium",
+      "The Great Wall of China"];
+
+  console.log("All Place Names");
+
+  for (const name of names) {
+    console.log(name);
+  }
+
+  console.log("");
+
+  const theNames = names.filter(name => name.startsWith("The"));
+
+  console.log("'The Place Names'")
+  for (const name of theNames) {
+    console.log(name);
+  }
+
+}
+
+main();unction main() {
   // Put your code here
 
 }
